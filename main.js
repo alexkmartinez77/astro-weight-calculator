8652d65a20b9e86daca0218596e12fbd07871f6b
Alex Martinez <alexkmartinez77@gmail.com>
2021-09-23 17:08:32 -0700

    Coded javascript to add planets to dropdown menu from planets array and coded the handleClickEvent function to acquire input values, calculate new weight based on planet and display the results.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-// Write your JavaScript code here! 
+// Multi-Level array of planets and their respective relative surface gravity
 var planets = [ 
     ['Pluto', 0.06], 
     ['Neptune', 1.148], 
@@ -13,18 +13,17 @@ var planets = [
     ['Mercury', 0.377], 
     ['Sun', 27.9] 
 ];
- 
-    // We are going to solve this by breaking the problem into three parts. 
-    // Programmers like automating things, we'll populate the HTML drop down options using code, 
-    // instead of having to type out all the values. 
-    // Create a function that does the some math and gives us the new weight. 
-    // Then create a function that responds when the user clicks on the button. 
 
-    // 1. Populate the dropdown element with the data found in the planets array. 
-    // The value of each option should be the planet's name. 
-    // Use the following built-in methods: 
-    // `.forEach` `document.createElement` `document.getElementById` `.appendChild` 
+var planetsList = document.getElementById('planets');
+//.forEach` `document.createElement` `document.getElementById` `.appendChild`
 
+planets.forEach(function(option) {
+  var newOption = document.createElement('option');
+
+  newOption.value = option[0];
+  newOption.innerHTML = option[0];
+  planetsList.appendChild(newOption);
+})
 // Returns planet factor
 function relativeSurfaceGravity(planetChosen){
     for (var i = 0; i < planets.length; i++) {
@@ -34,24 +33,23 @@ function relativeSurfaceGravity(planetChosen){
     }
 }
 
-// Returns planet weight given earth weight and planet name
+//Returns planet weight given earth weight and planet name
 function calculateWeight(weight, planetName){    
-    console.log("inside calculateWeight function")
     return (weight * relativeSurfaceGravity(planetName));
 } 
 
 function handleClickEvent(e) {
-     // 3. Create a variable called userWeight and assign the value of the user's weight. 
-
-     // 4. Create a variable called planetName and assign the name of the selected planet from the drop down. 
-
-    // 5. Create a variable called result and assign the value of the new calculated weight. 
-
-    // 6. Write code to display the message shown in the screenshot. 
-
+//Creates a variable called userWeight and assigns the value of the user's weight. 
+var userWeight = document.getElementById('user-weight').value;
+console.log(userWeight);
+//Creates a variable called planetName and assigns the name of the selected planet from the drop down. 
+var planetName = document.getElementById('planets').value;
+//Creates a variable called result and assigns the value of the new calculated weight. 
+var result = calculateWeight(userWeight, planetName);
+console.log(result);
+//Displays message indicating new weight on seledted planet
+document.getElementById('output').innerHTML = 'If you were on ' + planetName + ', you would weigh ' + result +'lbs!';
 } 
-
-    // 7. Set the #calculate-button element's onclick method to use the handleClickEvent function.
 
     // 8. Make it look nice by attaching  a style.css file to your index.html and writing some basic styling, 
     // feel free to add classes and id's to the HTML elements as you need, 
@@ -60,4 +58,3 @@ function handleClickEvent(e) {
 
     // Bonus Challenges 
     // 8. Reverse the drop down order so that the sun is first.
-
