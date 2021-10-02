8e681ccf3505c89a6a01fd61564ff5e5f3e9b2b7
Alex Martinez <alexkmartinez77@gmail.com>
2021-10-01 20:15:56 -0700

    Prettified the code.

--- main.js
+++ main.js
@@ -1,61 +1,62 @@
-
 // Multi-Level array of planets and their respective relative surface gravity
-var planets = [ 
-    ['Pluto', 0.06], 
-    ['Neptune', 1.148], 
-    ['Uranus', 0.917], 
-    ['Saturn', 1.139], 
-    ['Jupiter', 2.640], 
-    ['Mars', 0.3895], 
-    ['Moon', 0.1655], 
-    ['Earth', 1], 
-    ['Venus', 0.9032], 
-    ['Mercury', 0.377], 
-    ['Sun', 27.9] 
+var planets = [
+  ["Pluto", 0.06],
+  ["Neptune", 1.148],
+  ["Uranus", 0.917],
+  ["Saturn", 1.139],
+  ["Jupiter", 2.64],
+  ["Mars", 0.3895],
+  ["Moon", 0.1655],
+  ["Earth", 1],
+  ["Venus", 0.9032],
+  ["Mercury", 0.377],
+  ["Sun", 27.9],
 ];
 
 // Target the select element that has the 'planets id
-var planetsList = document.getElementById('planets');
+var planetsList = document.getElementById("planets");
 
 // Populate the dropdown menu with planet names in the planets array
-planets.forEach(function(option) {
-  var newOption = document.createElement('option');
+planets.forEach(function (option) {
+  var newOption = document.createElement("option");
 
   newOption.value = option[0];
   newOption.innerHTML = option[0];
   planetsList.appendChild(newOption);
-})
+});
 
 // Returns planet's multiplier
-function relativeSurfaceGravity(planetChosen){
-    for(const planet of planets){
-       const [pName, pmultiplier] = planet;
-        if(pName == planetChosen){
-            return pmultiplier;
-        }
+function relativeSurfaceGravity(planetChosen) {
+  for (const planet of planets) {
+    const [pName, pmultiplier] = planet;
+    if (pName == planetChosen) {
+      return pmultiplier;
     }
+  }
 }
 
 //Returns planet weight given earth weight and planet name
-function calculateWeight(weight, planetName){    
-    return (weight * relativeSurfaceGravity(planetName));
-} 
+function calculateWeight(weight, planetName) {
+  return weight * relativeSurfaceGravity(planetName);
+}
 
 function handleClickEvent(e) {
-//Creates a variable called userWeight and assigns the value of the user's weight. 
-var userWeight = parseFloat(document.getElementById('user-weight').value);
-//Creates a variable called planetName and assigns the name of the selected planet from the drop down. 
-var planetName = document.getElementById('planets').value;
+  //Creates a variable called userWeight and assigns the value of the user's weight.
+  var userWeight = parseFloat(document.getElementById("user-weight").value);
+  //Creates a variable called planetName and assigns the name of the selected planet from the drop down.
+  var planetName = document.getElementById("planets").value;
 
-console.log(typeof(userWeight), typeof(planetName));
+  console.log(typeof userWeight, typeof planetName);
 
-//Creates a variable called result and assigns the value of the new calculated weight. 
-var result = calculateWeight(userWeight, planetName).toFixed(2);
-//Displays message indicating new weight on seledted planet
-if(result == 0 || result == undefined || result == 'NaN'){
-    document.getElementById('output').innerHTML = 'Please enter your weight in lbs.';
-}else{
-    document.getElementById('output').innerHTML = `If you were to live on <span class="planetNames">${planetName}</span>, you would weigh <span class="planetNames">${result}</span> lbs!`;
-
+  //Creates a variable called result and assigns the value of the new calculated weight.
+  var result = calculateWeight(userWeight, planetName).toFixed(2);
+  //Displays message indicating new weight on seledted planet
+  if (result == 0 || result == undefined || result == "NaN") {
+    document.getElementById("output").innerHTML =
+      "Please enter your weight in lbs.";
+  } else {
+    document.getElementById(
+      "output"
+    ).innerHTML = `If you were to live on <span class="planetNames">${planetName}</span>, you would weigh <span class="planetNames">${result}</span> lbs!`;
+  }
 }
-} 
